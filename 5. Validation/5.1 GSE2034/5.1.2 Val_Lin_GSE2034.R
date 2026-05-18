ca735f05db1c182df87d21f7b71afa43c907e7c0
pablojimenezferia <pablojimenez@lasallistas.org.mx>
2026-05-18 08:47:06 -0600

    Refactor prediction and AUC calculations for GSE2034
    Updated the prediction phase to include row names and modified AUC calculations to include additional time points. Adjusted plotting functions and labels for improved clarity and accuracy.

--- 5. Validation/5.1 GSE2034/5.1.2 Val_Lin_GSE2034.R
+++ 5. Validation/5.1 GSE2034/5.1.2 Val_Lin_GSE2034.R
@@ -4,7 +4,8 @@
 # This is the predict phase
 
 gse2034_results <- predict(final_fit, new_data = proof_genes_pt_gse2034, type = "linear_pred") %>%
-  bind_cols(proof_genes_pt_gse2034)
+  bind_cols(proof_genes_pt_gse2034 %>% 
+              rownames_to_column("file_name"))
 
 
 # 6.3 To get the p value
@@ -68,80 +69,57 @@ library(timeROC)
 
 # Area under the curve per time
 
-res_auc <- timeROC(T = gse2034_results$EVENT_MON,
+res_auc.gse2034 <- timeROC(T = gse2034_results$EVENT_MON,
                    delta = gse2034_results$ EVENT_STAT,
                    marker = -gse2034_results$pred_z,
                    cause = 1, # The event code
-                   times = c(36, 60, 120), # 3, 5, and 10 years
+                   times = c(12, 36, 60, 72, 120), # 3, 5, and 10 years
                    iid = TRUE)
 
 # 6.9.2 View the AUC values
 
-res_auc_gse2034 <- res_auc$AUC %>% 
+auc_gse2034 <- res_auc.gse2034$AUC %>% 
   as.data.frame()
 
 # View the AUC values
 
 print(res_auc$AUC)
 
-# 6.9.3 Combine all time points into a long dataframe
 
-plot_roc.gse2034 <- map_df(c(12, 36, 60, 120), function(i) { # This functions as a for loop
-  time_label <- paste0("t=", i)
-  
-  data.frame(
-    FP = res_auc$FP[, time_label],
-    TP = res_auc$TP[, time_label],
-    Time = factor(i),
-    data_set = "GSE2034"
-  )
-})
 gse2034_results <- 
   gse2034_results %>% 
   mutate(EVENT_STAT = factor(EVENT_STAT))
 
 global_roc.gse2034 <- roc_curve(gse2034_results,
-                                EVENT_STAT,
-                                .pred_linear_pred) %>%
+                             EVENT_STAT,
+                             .pred_linear_pred
+) %>%
   mutate(label = "GSE2034")
 
 
-# 6.9.4 Extract AUC and SE for each time point
+# 3.2 Combine all time points into a long dataframe
 
-auc_table.gse2034 <- data.frame(
-  Time = c(12, 36, 60, 120),
-  `AUC (%)` = round(as.numeric(res_auc$AUC[c("t=12", "t=36", "t=60", "t=120")]), 4),
-  `SE` = res_auc$times
-)
+plot_roc.gse2034 <- map_df(c(12, 36, 60, 72, 120), function(i) { # This functions as a for loop
+  time_label <- paste0("t=", i)
+  
+  data.frame(
+    FP = res_auc.gse2034$FP[, time_label],
+    TP = res_auc.gse2034$TP[, time_label],
+    Time = factor(i),
+    data_set = "GSE2034"
+  )
+})
 
-# 6.9.5 Labels
 
-legend_labels.gse2034 <- paste0("t=", auc_table.gse2034$Time, " (AUC: ", 100 * auc_table.gse2034$AUC..., "%)")
-
-# 6.9.6 Labels for faceted plot
+# 3.3 Labels for faceted plot
 
 facet_labels.gse2034 <- data.frame(
-  Time = factor(c(12, 36, 60, 120)),
-  AUC_Text = paste0("AUC: ", 100 * round(as.numeric(res_auc$AUC[1:4]), 3), "%"),
+  Time = factor(c(12, 36, 60, 72, 120)),
+  AUC_Text = paste0("AUC: ", 100 * round(as.numeric(res_auc.gse2034$AUC[1:5]), 3), "%"),
   data_set = "GSE2034"
 )
 
-# 6.9.7 Plot with overlap
-
-ggplot(plot_roc.gse2034, aes(x = FP, y = TP, color = Time)) +
-  geom_line(linewidth = 1) +
-  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "grey50") +
-  theme_minimal() +
-  labs(
-    title = "Time-Dependent ROC Curves",
-    subtitle = "Comparing model performance across different horizons",
-    x = "1 - Specificity (FP)",
-    y = "Sensitivity (TP)",
-    color = "Time Point"
-  ) +
-  scale_color_viridis_d(labels = legend_labels.gse2034) 
-
-# 6.9.8 Plot with facet
+# 3.4 Plot with facet
 
 ggplot(plot_roc.gse2034, aes(x = FP, y = TP)) +
   geom_line(color = "darkblue", linewidth = 1) +
@@ -156,6 +134,7 @@ ggplot(plot_roc.gse2034, aes(x = FP, y = TP)) +
   geom_text(data = facet_labels.gse2034, 
             aes(x = 0.75, y = 0.1, label = AUC_Text), 
             size = 4, fontface = "bold")
+
 
 
 
@@ -186,7 +165,198 @@ num_param_compare <- c(9:21)
 
 cat(paste0("The signature got a C-score of ", round(c_index_results.2034$concordance, 2)),
     paste0("an HR of ", round(summary_gse2034$coefficients[2], 2), " (CI 95% of ", round(summary_gse2034$conf.int[3], 2), " - ", round(summary_gse2034$conf.int[4], 2), " pval ", summary_gse2034$coefficients[5], ")"),
-    paste0("AUC at 3 years of ", round(res_auc_gse2034[1,], 2), " at 5 years of ", round(res_auc_gse2034[2,], 2), " and at 6 years of "),
+    paste0("AUC at 3 years of ", round(auc_gse2034[1,], 2), " at 5 years of ", round(auc_gse2034[2,], 2), " and at 6 years of "),
     paste0("As an independence factor it has an HR of ", round(independent_prog.gse2034$estimate[independent_prog.gse2034$term == "SCORE"], 2), " (CI 95% of ", round(independent_prog.gse2034$conf.low[independent_prog.gse2034$term == "SCORE"], 2), " - ", round(independent_prog.gse2034$conf.high[independent_prog.gse2034$term == "SCORE"], 2), " pval of ", independent_prog.gse2034$p.value[independent_prog.gse2034$term == "SCORE"], ")"),
     sep = ". "
 )
+
+
+
+
+
+
+# 4.5 Forest plot ignoring values that tend to infinite
+
+independent_prog.gse2034 %>%
+  filter(estimate > 0.0001,
+         conf.high < 100) %>%
+  mutate(
+    term = reorder(term, p.value),
+    significant = p.value < 0.05
+  ) %>%
+  ggplot(aes(x = estimate, y = term, color = significant)) +
+  geom_point() +
+  geom_errorbarh(aes(xmin = conf.low, xmax = conf.high), height = 0.5, linewidth = 1.2) +
+  geom_vline(xintercept = 1, linetype = "dashed") +
+  scale_x_log10() +
+  theme_minimal()
+
+
+# 6.- Outlier analysis ----------------------------------------------------
+
+
+# 6.1 Utilize the fitted object to make predictions based on time
+
+# 6.1.1 Change to numeric 
+
+gse2034_results <- 
+  gse2034_results %>% 
+  mutate(EVENT_STAT = as.numeric(EVENT_STAT))
+
+# 6.1.2 Augment on different time points
+
+model_diagnostics <- augment(
+  final_fit, 
+  new_data = gse2034_results, 
+  eval_time = c(36, 60, 120) # Times in months (3, 5, 10 years)
+)
+
+# 6.2 Create list to then add the results
+
+list <- list()
+
+
+#> 6.3 For loop that at each desired time point calculates the bias scores, extracts patients with high bias scores
+#> observe metadata of outlier patients and plot the distribution of the prediction with the actual event time
+
+for (i in c(36, 60, 120)) {
+  
+  eval_time <- i
+  
+  # 6.3.1 Unnest the predictions and find the biggest outliers on a set point and event
+  
+  outliers <- model_diagnostics %>%
+    dplyr::select(file_name, EVENT_MON, EVENT_STAT, .pred) %>%
+    unnest(.pred) %>%
+    filter(.eval_time == eval_time) %>% 
+    arrange(desc(.pred_survival)) # Siunce our signature as it goes up, the predicted mortality goes down we see which patients died early who were predicted to die late or survive
+  
+  
+  # 6.3.2 Object with metadata and score characteristics
+  
+  outlier_summary <- outliers %>%
+    inner_join(metadata.gse.2034_er_pos, by = "file_name", suffix = c("", ".drop")) %>%
+    dplyr::select(
+      file_name, 
+      .pred_survival, 
+      EVENT_STAT, 
+      EVENT_MON, 
+      BRAIN_REL, 
+      .eval_time
+    ) 
+  
+  
+  # 6.3.3 Create bias score for defined time
+  
+  outliers_bias <- outlier_summary %>%
+    mutate(
+      bias_score = (((1 - EVENT_STAT) - .pred_survival) ^ 2) * ((EVENT_MON - .eval_time) * ( 1 - 2 * (EVENT_STAT)))
+    ) %>%
+    arrange(desc(bias_score))
+  
+  # 6.4 Identify the highest bias patients
+  
+  # 6.4.1 Obtain mean and sd and then filter baed on patients higher than determined SD
+  
+  extreme_outliers <- 
+    outliers_bias %>% 
+    mutate(mean_bias = mean(bias_score),
+           sd_bias = sd(bias_score)) %>% 
+    filter(bias_score > (mean_bias + 1 * sd_bias))
+  
+  # 6.4.2 Obtain their IDs
+  
+  top_bias_ids <- extreme_outliers$file_name
+  
+  print(length(top_bias_ids))
+  
+ 
+  # 6.5.1 Add a column identifying patients as top bias or not
+  
+  outliers <- 
+    outliers %>% 
+    mutate(quadrant = case_when(
+      file_name %in% top_bias_ids & EVENT_STAT == 0 ~ 2,
+      file_name %in% top_bias_ids & EVENT_STAT == 1 ~ 1,
+      TRUE ~ 0
+    ))
+  
+  print(outliers %>% 
+          group_by(EVENT_STAT) %>% 
+          dplyr::count(quadrant))
+  
+  # 6.5.2 Plot
+  
+  theme_embedded <- theme_classic(base_size = 25) + 
+    theme(
+      legend.position = c(0.95, 0.3), # Adjust coordinates (x, y) from 0 to 1
+      legend.background = element_rect(fill = alpha("white", 0.5))
+    )
+  
+  # 6.5.2.1 Plot colored by EVENT_STAT
+  
+  p1 <- ggplot(outliers, aes(x = EVENT_MON, y = .pred_survival, color = factor(EVENT_STAT), shape = factor(EVENT_STAT))) +
+    geom_point(size = 2, alpha = 0.7) + # Increased size and opacity
+    stat_ellipse(type = "t", level = 0.95) + # Adds 95% confidence ellipse
+    geom_vline(xintercept = eval_time, linetype = "dashed", color = "red") +
+    scale_color_viridis_d() + 
+    labs(
+      title = "Event Status Distribution",
+      x = paste0("Actual Event Time (Months)", eval_time),
+      y = "Predicted Survival",
+      color = "Event Stat",
+      shape = "Event Stat"
+    ) +
+    theme_embedded
+  
+  # 6.5.2.2 Plot colored by quadrant
+  
+  p2 <- ggplot(outliers, aes(x = EVENT_MON, y = .pred_survival, color = factor(quadrant), shape = factor(EVENT_STAT))) +
+    geom_point(size = 2, alpha = 0.7) +
+    stat_ellipse(aes(group = quadrant), type = "t", level = 0.95) + 
+    geom_vline(xintercept = eval_time, linetype = "dashed", color = "red") +
+    scale_color_viridis_d() + 
+    labs(
+      title = "Quadrant Analysis",
+      x = paste0("Actual Event Time (Months)", eval_time),
+      y = "Predicted Survival",
+      color = "Quadrant",
+      shape = "Event Stat"
+    ) +
+    theme_embedded
+  
+  # 6.5.3 Combine and stack
+  
+  print( p1 + p2)
+  
+  list[[i]] <- top_bias_ids
+  
+}
+
+
+
+# 7.- Other scores --------------------------------------------------------
+
+# 7.1 Brier score
+
+eval_results.gse2034 <- final_fit %>%
+  augment(new_data = proof_genes_pt_gse2034, eval_time = c(36, 60, 120)) 
+
+performance.gse2034 <- eval_results.gse2034 %>%
+  brier_survival(truth = surv_obj, .pred)
+
+print(performance.gse2034)
+
+# 7.2 Martingale and Schofeild residuals
+
+cox.zph(cox_model.gse2034)
+
+
+
+ggcoxzph(cox.zph(cox_model.gse2034))
+
+
+
+ggcoxdiagnostics(cox_model.gse2034, type = "martingale",
+                 linear.predictions = FALSE, ggtheme = theme_bw())
+
