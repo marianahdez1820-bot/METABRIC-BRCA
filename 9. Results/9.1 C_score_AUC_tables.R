7788b8f3a843e4c39e2950847c8e362da56a5246
pablojimenezferia <pablojimenez@lasallistas.org.mx>
2026-06-22 13:31:41 -0600

    Refactor c_index_df and auc_df data processing

--- 9. Results/9.1 C_score_AUC_tables.R
+++ 9. Results/9.1 C_score_AUC_tables.R
@@ -1,5 +1,7 @@
+library(magrittr)
 
-c_index_df <- bind_rows(c_index_summary.tcga, c_index_summary.gse96058, c_index_summary)
+
+c_index_df <- bind_rows(c_index_summary, c_index_summary.tcga, c_index_summary.gse96058)
 
 c_index_df <- 
   c_index_df %>% 
@@ -9,24 +11,24 @@ c_index_df <-
 c_index_df <- 
   c_index_df %>% 
   mutate(
-    across(where(is.numeric) & !p_value, \(x) round(x, digits = 2)),
-    across(p_value, \(x) round(x, digits = 12))
+    across(where(is.numeric) & !p_value, \(x) round(x, digits = 3)),
+    p_value = format(p_value, scientific = TRUE, digits = 3)
   )
 
 flextable(c_index_df) %>%
-  autofit() 
+  autofit()
 
 
-auc_df <- bind_rows(auc_ci.tcga, auc_ci.gse96058, auc_ci)
+auc_df <- bind_rows(auc_ci, auc_ci.tcga, auc_ci.gse96058)
 
 auc_df <- 
   auc_df %>% 
   relocate(data_set, time, AUC, conf_int_low95, conf_int_high95, z_stat, p_value) %>% 
   dplyr::select(- SE) %>% 
   mutate(
-    across(where(is.numeric) & !p_value, \(x) round(x, digits = 2)),
-    across(p_value, \(x) round(x, digits = 12))
+    across(where(is.numeric) & !p_value, \(x) round(x, digits = 3)),
+    p_value = format(p_value, scientific = TRUE, digits = 3)
   )
-
 flextable(auc_df) %>%
   autofit() 
+
