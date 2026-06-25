69b6d6483b28befa709a16a38fc60c06465f1b3c
pablojimenezferia <pablojimenez@lasallistas.org.mx>
2026-06-25 12:44:50 -0600

    Remove RDS file saving from Rec_split script
    Removed saving of RDS files for counts data and metadata.

--- 2_Feature_selection/2_1_2_Rec_split.R
+++ 2_Feature_selection/2_1_2_Rec_split.R
@@ -19,10 +19,3 @@ test_rec.id <- metadata_rec_test$PATIENT_ID
 
 length(train_rec.id)
 length(test_rec.id)
-
-
-saveRDS(counts_data, "counts_data.rds")
-saveRDS(metadata_rec_train, "metadata_rec_train.rds")
-saveRDS(metadata_rec_test, "metadata_rec_test.rds")
-saveRDS(train_rec.id, "train_rec_id.rds")
-saveRDS(test_rec.id, "test_rec_id.rds")
